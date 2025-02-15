ad4af63532bb4eded8b17a4bbda04eefef2c3b65
Susana Alarcón <142707069+Susi1213@users.noreply.github.com>
2025-02-14 20:52:40 -0500

    nueva actualización

--- app.py
+++ app.py
@@ -1,10 +1,34 @@
-from flask import Flask
+from flask import Flask , request
 
-app = Flask(__name__)
+app = Flask ( __name__ )
 
-@app.route('/')
-def home():
-    return "¡Bienvenida Personalizado!"
 
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route ( '/' )
+def hello_world():
+    return 'Hello World clase encuentro!!'
+
+
+# Nueva ruta para mostrar un mensaje personalizado
+@app.route ( '/usuario/<nombre>' )
+def usuario(nombre):
+    return f'Bienvenido, {nombre}!'
+
+
+# Ruta para mostrar el formulario
+@app.route ( '/usuario/' , methods = [ 'GET' , 'POST' ] )
+def usuario_form():
+    if request.method == 'POST':
+        nombre = request.form [ 'nombre' ]
+        return f'Bienvenido, {nombre}!!!'
+
+    return '''
+    <form method="post">
+        <label for="nombre">Ingresa tu nombre:</label>
+        <input type="text" id="nombre" name="nombre" required>
+        <button type="submit">Enviar</button>
+    </form>
+    '''
+
+
+if __name__ == '__main__':
+    app.run ( debug = True )
